e2f63df5c34acd7544ed6408a5e431ae6dca777e
linh <trankhanhlinh.28102000@gmail.com>
2020-08-03 11:04:51 +0700

    Test

---
+++ linh/Episerver/Info.md
@@ -0,0 +1,3 @@
+Account: longhai
+
+Password: Admin1_
---
+++ linh/Episerver/Models/Pages/TestPage.cs
@@ -0,0 +1,65 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EpiserverSite.Models;
+using EpiserverSite.Models.Pages;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EpiserverSite.Models.Pages
+{
+    [SiteContentType(DisplayName = "Test Page", GUID = "0e3d4805-69ae-4e14-942e-e83e094ac71e", Description = "")]
+    public class TestPage : StandardPage
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 325)]
+        [CultureSpecific]
+
+        public virtual XhtmlString Heading
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Avatar
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Information
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Details1
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Image1
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Details2
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Image2
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Details3
+        {
+            get; set;
+        }
+
+        public virtual XhtmlString Image3
+        {
+            get; set;
+        }
+    }
+}
